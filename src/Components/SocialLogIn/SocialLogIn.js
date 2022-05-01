daf95125a66d604ca123337edcd37131f0dfe20b
shely25 <shely.ece.25@gmail.com>
2022-05-02 00:00:48 +0600

    some UI designed

--- src/Components/SocialLogIn/SocialLogIn.js
+++ src/Components/SocialLogIn/SocialLogIn.js
@@ -3,6 +3,7 @@ import { GithubLoginButton, GoogleLoginButton } from "react-social-login-buttons
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
+import './SocialLogIn.css'
 
 const SocialLogIn = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
@@ -21,18 +22,19 @@ const SocialLogIn = () => {
     }
     return (
         <div>
-            <div>
-                <div className='d-flex justify-content-center align-items-center'>
-                    <div style={{ border: '1px solid blue', width: '150px', borderRadius: '4px' }}></div>
-                    <p style={{ margin: '1px 8px 0 8px' }}>Or</p>
-                    <div style={{ border: '1px solid blue', width: '150px', borderRadius: '4px' }}></div>
-                </div>
-                {errorElement}
-                <GoogleLoginButton onClick={() => signInWithGoogle()} className='w-25 mx-auto my-3' />
-                <GithubLoginButton onClick={() => signInWithGithub()} className='w-25 mx-auto' />
-            </div >
-
+            <div className='d-flex justify-content-center align-items-center'>
+                <div style={{ border: '1px solid blue', width: '150px', borderRadius: '4px' }}></div>
+                <p style={{ margin: '1px 8px 0 8px' }}>Or</p>
+                <div style={{ border: '1px solid blue', width: '150px', borderRadius: '4px' }}></div>
+            </div>
+            {errorElement}
+            <div className='Width'>
+                <GoogleLoginButton onClick={() => signInWithGoogle()} />
+                <GithubLoginButton onClick={() => signInWithGithub()} />
+            </div>
         </div >
+
+
     );
 };
 
